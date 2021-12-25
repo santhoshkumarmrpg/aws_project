60fb6e23f58f60669f802f430432a5e4beb3eb36
santhoshkumarmrpg <95461998+santhoshkumarmrpg@users.noreply.github.com>
2021-12-25 16:27:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 1.0.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0902b0be1c5a22a55"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "webInstance"
+  }
+}
