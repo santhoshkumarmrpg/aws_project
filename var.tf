ba1d254adc2a9e630fc5851d52d0d5e0bc1ea91a
santhoshkumarmrpg <95461998+santhoshkumarmrpg@users.noreply.github.com>
2022-01-02 19:15:09 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "access_key"{
+  default = "AKIAZPROUA3NJ5GN7WNR"
+}
+variable "secret_key"{
+  default = "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"
+}
+variable "region"{
+  default = "ap-south-1"
+}
